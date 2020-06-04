9b6af770a8449c76c8c37fb21b7d236df988bda6
ferrisbueller66 <github email address>
2020-06-03 22:35:44 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,15 @@
 module SongsHelper
 
+
+    def display_artist(song)
+        #is an artist associated with a song?
+        #if yes, show a link to artist' show page
+        # if no, show link to song's edit page with link text "Add Artist"
+        if song.artist != nil
+            link_to song.artist.name, song.artist
+            
+        else 
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 end
